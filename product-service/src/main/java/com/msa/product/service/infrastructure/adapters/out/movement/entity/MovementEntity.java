58153909699f29653e7965f2c311b6d08dda1f63
bvegam <vegabryam40@gmail.com>
2024-11-02 08:15:50 -0500

    Generate domain class and entity structure for database.
    Modify docker-compose.yml to add product database

---
+++ product-service/src/main/java/com/msa/product/service/infrastructure/adapters/out/movement/entity/MovementEntity.java
@@ -0,0 +1,42 @@
+package com.msa.product.service.infrastructure.adapters.out.movement.entity;
+
+import com.msa.product.service.domain.models.enums.MovementTypeEnum;
+import com.msa.product.service.infrastructure.adapters.out.account.entity.AccountEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "T_MOVEMENTS")
+public class MovementEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MOV_ID", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "MOV_DATE", nullable = false)
+    private LocalDateTime date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "MOV_TYPE", nullable = false)
+    private MovementTypeEnum movementType;
+
+    @Column(name = "MOV_VALUE", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "MOV_BALANCE", nullable = false)
+    private BigDecimal balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MOV_ACC_ID", nullable = false)
+    private AccountEntity account;
+
+}
