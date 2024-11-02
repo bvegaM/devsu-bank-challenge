da4c5e26ab5068a84fb31d1278a7fd6e07773db0
bvegam <vegabryam40@gmail.com>
2024-11-02 09:36:26 -0500

    Generate endpoint to get account by id

--- product-service/src/main/java/com/msa/product/service/infrastructure/adapters/out/account/AccountAdapter.java
+++ product-service/src/main/java/com/msa/product/service/infrastructure/adapters/out/account/AccountAdapter.java
@@ -1,12 +1,15 @@
 package com.msa.product.service.infrastructure.adapters.out.account;
 
 import com.msa.product.service.application.ports.GenericOPort;
+import com.msa.product.service.domain.exceptions.NotFoundServiceException;
 import com.msa.product.service.domain.models.Account;
 import com.msa.product.service.infrastructure.adapters.out.account.entity.AccountEntity;
 import com.msa.product.service.infrastructure.adapters.out.account.mapper.AccountEntityMapper;
 import com.msa.product.service.infrastructure.adapters.out.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -23,7 +26,9 @@ public class AccountAdapter implements GenericOPort<Account, Integer> {
 
     @Override
     public Account getById(Integer id) {
-        return null;
+        Optional<AccountEntity> client = repository.findActiveAccountById(id);
+        return client.map(mapper::toAccount)
+                .orElseThrow(() -> new NotFoundServiceException("Account not found"));
     }
 
     @Override
